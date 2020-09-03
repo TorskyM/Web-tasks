1da70706352da9c8422cf90bbd63030ea7b4e497
unknown <mikhailotorsky@gmail.com>
2020-09-03 22:18:18 +0300

    Validation fix

--- web-task-1/admin/admin.js
+++ web-task-1/admin/admin.js
@@ -1,27 +1,23 @@
-const addNewsButton = document.querySelector('#add-news-button');
-const newsTitle = document.querySelector('.news-text-title');
-const newsText = document.querySelector('.news-text-content');
-const wrapper = document.querySelector('.news-text-wrapper');
-const titleErrorAnchor = document.querySelector('.error-message-title');
-const contentErrorAnchor = document.querySelector('.error-message-content');
+const addNewsButton = document.querySelector("#add-news-button");
+const newsTitle = document.querySelector(".news-text-title");
+const newsText = document.querySelector(".news-text-content");
+const wrapper = document.querySelector(".news-wrapper");
+const titleErrorAnchor = document.querySelector(".error-message-title");
+const contentErrorAnchor = document.querySelector(".error-message-content");
 
-const createNews = document.createElement('div');
-const titleErrorMessage = document.createElement('p');
-const contentErrorMessage = document.createElement('p');
+const createNews = document.createElement("div");
+const titleErrorMessage = document.createElement("p");
+const contentErrorMessage = document.createElement("p");
+const notification = document.createElement("p");
 
-addNewsButton.addEventListener('click', clickHanler);
+addNewsButton.addEventListener("click", clickHanler);
 
-createNews.className = 'news-item';
-titleErrorMessage.className = 'error';
-titleErrorMessage.textContent = 'Enter at least 10 characters';
-contentErrorMessage.className = 'error';
-contentErrorMessage.textContent = 'Enter at least 15 characters';
-createNews.innerHTML = `
-  <img src='../assets/news-image.png' alt='news image' class='news-image'>
-  <div class='news-content'>
-    <h3 class='news-title'>${newsTitle.value}</h3>
-    <p class='news-text'>${newsText.value}</p>
-  </div>`;
+createNews.className = "news-item";
+titleErrorMessage.className = "error";
+titleErrorMessage.textContent = "Enter at least 10 characters";
+contentErrorMessage.className = "error";
+contentErrorMessage.textContent = "Enter at least 15 characters";
+notification.textContent = "News was added successfully, news data is:";
 
 const validateTitle = () => {
   if (newsTitle.value && newsTitle.value.length >= 10) {
@@ -30,7 +26,7 @@ const validateTitle = () => {
     return true;
   } else {
     titleErrorAnchor.append(titleErrorMessage);
-    newsTitle.className += ' invalid';
+    newsTitle.className += " invalid";
   }
 };
 
@@ -41,24 +37,34 @@ const validateContent = () => {
     return true;
   } else {
     contentErrorAnchor.append(contentErrorMessage);
-    newsText.className += ' invalid';
+    newsText.className += " invalid";
 
     return false;
   }
 };
 
 const setValid = () => {
-  if (document.querySelector('.error')) {
-    document.querySelector('.error').style.display = 'none';
-    newsTitle.className = 'new-comment-text';
-    newsText.className = 'news-text-content';
+  if (document.querySelectorAll(".error")) {
+    document
+      .querySelectorAll(".error")
+      .forEach((el) => (el.style.display = "none"));
+    newsTitle.className = "new-comment-text";
+    newsText.className = "news-text-content";
   }
 };
 
 function clickHanler() {
   if (validateTitle() && validateContent()) {
+    createNews.innerHTML = `
+  <img src='../assets/news-image.png' alt='news image' class='news-image'>
+  <div class='news-content'>
+    <h3 class='news-title'>${newsTitle.value}</h3>
+    <p class='news-text'>${newsText.value}</p>
+  </div>`;
+    wrapper.append(notification);
     wrapper.append(createNews);
-    newsTitle.value = '';
-    newsText.value = '';
+
+    newsTitle.value = "";
+    newsText.value = "";
   }
 }
